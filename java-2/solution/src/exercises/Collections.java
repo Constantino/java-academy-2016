e7f2542bcafcb053fcf285be9f56cca4a670a5e9
Constantino Mora <constantinomora@gmail.com>
2016-09-19 03:06:07 -0500

    2.-Set and Ordering

--- java-2/solution/src/exercises/Collections.java
+++ java-2/solution/src/exercises/Collections.java
@@ -1,13 +1,14 @@
 package exercises;
 
-
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.LinkedList;
 import java.util.Queue;
-
 
 public class Collections {
 
@@ -37,9 +38,50 @@ public class Collections {
 		myQueue.add("e");
 		
 		loopThrough(myQueue);
+		
+		System.out.println("**** 2 ****");
+		
+		/*
+		 * TreeSet Iterates in Natural Order, it does that in this case
+		 * alphabetically.
+		 * */
+		TreeSet<String> tree = new TreeSet<String>();
+		tree.add("Jake");
+		tree.add("Sansa");
+		tree.add("Robert");
+		tree.add("Marisa");
+		tree.add("Abcd");
+		tree.add("Arthur");
+		tree.add("Kasey");
+		System.out.println(tree);
+		
+		/*
+		 * Order depends on hashcodes, because internally creates a HasMap, hence order
+		 * unpredictable.
+		 * */
+		Set<String> names = new HashSet<String>();
+		names.add("Jake");
+		names.add("Robert");
+		names.add("Marisa");
+		names.add("ZZ");
+		names.add("Kasey");
+		System.out.println(names);
+		
+		
+		/*
+		 * Ordered as added
+		 * */
+		LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+		lhs.add("Jake");
+		lhs.add("Robert");
+		lhs.add("Marisa");
+		lhs.add("ZZ");
+		lhs.add("Kasey");
+		System.out.println(lhs);
+		
 	}
 	
-	public static <T> void loopThrough(Iterable<T> myCollection){
+	public static <T> void loopThrough(Collection<T> myCollection){
 		
 		for(T e : myCollection){
 			System.out.println(e);
